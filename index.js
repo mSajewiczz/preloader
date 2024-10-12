8cb2de2c054a662e11dc330ecb383f65bc7b24cf
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2024-10-12 22:41:47 +0200

    Added variables in css, added describe of program work in html

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// function isLoaded() {
-//     console.info("DOM loaded");
-//   }
-  
-//   if (document.readyState === "loading") {
-    
-//     console.log("Loading");
-//     document.addEventListener("DOMContentLoaded", isLoaded());
-//   } 
-//   else {
-//     isLoaded();
-//   }
-
-
 const preloader = document.querySelector('#preloader');
 
 preloader.classList.add('show-preloader');
