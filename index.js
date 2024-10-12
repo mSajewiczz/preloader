bd8099a7401656d5e6408eca3064383849e7537a
Mikołaj Sajewicz <mikolaj.sajewicz11@gmail.com>
2024-10-12 22:34:04 +0200

    Updated preloading - final version

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-function isLoaded() {
-    console.info("DOM loaded");
-  }
+// function isLoaded() {
+//     console.info("DOM loaded");
+//   }
   
-  if (document.readyState === "loading") {
-
-    console.log("Loading");
-    document.addEventListener("DOMContentLoaded", isLoaded());
+//   if (document.readyState === "loading") {
     
-  } else {
-    isLoaded();
-  }
+//     console.log("Loading");
+//     document.addEventListener("DOMContentLoaded", isLoaded());
+//   } 
+//   else {
+//     isLoaded();
+//   }
+
+
+const preloader = document.querySelector('#preloader');
+
+preloader.classList.add('show-preloader');
+
+window.addEventListener('load', function () {
+	setTimeout(function(){
+    	preloader.classList.remove('show-preloader');
+	}, 2000);
+});
